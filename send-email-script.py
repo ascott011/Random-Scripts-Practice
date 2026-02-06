ea08ad9e1ba63140abbdc3a8a05422bef548bae2
Alex Scott <ascott011uk@gmail.com>
2026-02-06 15:42:23 -0500

    environment setup. first few lines

--- send-email-script.py
+++ send-email-script.py
@@ -1,2 +1,12 @@
 # send email script
 
+import smtplib
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
